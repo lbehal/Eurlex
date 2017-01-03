4cc451e36874ff43e20de18fd0e240adfa7bd1d2
lbehal <lbehal@users.noreply.github.com>
2017-01-04 00:10:39 +0100

    ecli

--- EurlexExt.user.js
+++ EurlexExt.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Eurlex
 // @namespace    eu_01
-// @version      0.3
+// @version      0.4
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
 // @require      https://raw.githubusercontent.com/lbehal/Eurlex/master/tooltipster.bundle.min.js
 // @resource     tooltipster_css https://raw.githubusercontent.com/lbehal/Eurlex/master/tooltipster.bundle.min.css
@@ -36,37 +36,9 @@
 		return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
 	}
 	
-	$("a").each(function() {
-		
-		var el = $(this);
-		var linkText = el.text().replace(/\u00a0/g, " ");
-		var href = el.attr('href');
-		var textOk = linkMatch.test(linkText);
-		if(ojUriMatch.test(href) && textOk === true)
-		{
-			var match = ojUriMatch.exec(href);
-			var oj = match[1];
-			var year = match[2];
-			var num = match[3];
-			var pn = linkMatch.exec(linkText)[1];
-
-			var pageNum = ('0000'+pn).slice(-4);
-			
-			var sparqlQuery = `prefix cdm: <http://publications.europa.eu/ontology/cdm#> select distinct ?celex where 
-					{
-					  ?oj  cdm:official-journal_number '${num}'^^xsd:positiveInteger.
-					  ?oj cdm:publication_general_date_publication ?dp.
-					  BIND(YEAR(xsd:datetime(?dp)) as ?year)
-					  filter(?year = ${year})
-					  ?oj  cdm:official-journal_part_of_collection_document <http://publications.europa.eu/resource/authority/document-collection/OJ-${oj}>.
-					  ?w cdm:resource_legal_id_celex ?celex.
-					  ?w cdm:resource_legal_published_in_official-journal ?oj.  
-					  ?e cdm:expression_belongs_to_work ?w.
-					  ?m cdm:manifestation_manifests_expression ?e.
-					  ?m cdm:manifestation_official-journal_part_page_first '${pageNum}'^^xsd:string.
-					}
-					`;
-			var queryParams = 
+	function sparqlQuery(sparqlQuery, onLoad)
+	{
+		var queryParams = 
 				{
 					query : sparqlQuery,
 					format: "application/sparql-results+json",
@@ -84,51 +56,80 @@
 					"Accept": "application/json, text/javascript, */*; q=0.01"
 				},
 				onload: function(response) {
-					try {
-						
+					try {							
 						var result = JSON.parse(response.responseText);
-						if(result.results === null || result.results.bindings.length === 0 || result.results.bindings["0"].celex === undefined) return;
-						console.log(result);
-						var celex = result.results.bindings["0"].celex.value;
-						var celexMsg = celex+" zkopírován do schránky";
-						if(typeof celex === undefined) return;
-						console.log(celex);
-						
-						//create button with celexid and set it up for clipboard copy..					
-						var celexEls = $(`<button class="btn" style="margin-left:5px" data-clipboard-text="${celex}">${celex}</button>`);
-						
-						
-						//noteEl.append(celexEls);
-						celexEls.insertAfter(el);
-						
-						debugger;
-						var noteEl = el.parent("p.note");	
-						if(noteEl !== null)
-						{
-							var noteLink = noteEl.children('a:first-child');
-							var noteLinkId = noteLink.attr('id');
-							$(`a[href='#${noteLinkId}']`).each(function() 
-															   {
-								//this is a reference of changed notelink.
-								//we will update tooltipster value for all these since tooltipster does copy the html on init.
-
-								var instance = $(this).tooltipster();
-								//instance.content(noteEl);
-								$(this).tooltipster('content', noteEl);
-
-							});
-						}
+						if(result.results === null || result.results.bindings.length === 0) return;
+						onLoad(result.results.bindings);						
 					}
 					catch(err) {
 						console.log(err);
 						debugger;
-					}
-					
+					}					
 				}
-			});		
+			});	
+	}
+	
+	//add celex buttons to all OJ links 
+	$("a").each(function() {		
+		var el = $(this);
+		var linkText = el.text().replace(/\u00a0/g, " ");
+		var href = el.attr('href');
+		var textOk = linkMatch.test(linkText);
+		if(ojUriMatch.test(href) && textOk === true)
+		{
+			var match = ojUriMatch.exec(href);
+			var oj = match[1];
+			var year = match[2];
+			var num = match[3];
+			var pn = linkMatch.exec(linkText)[1];
+
+			var pageNum = ('0000'+pn).slice(-4);
+			
+			var s_query = `prefix cdm: <http://publications.europa.eu/ontology/cdm#> select distinct ?celex where 
+					{
+					  ?oj  cdm:official-journal_number '${num}'^^xsd:positiveInteger.
+					  ?oj cdm:publication_general_date_publication ?dp.
+					  BIND(YEAR(xsd:datetime(?dp)) as ?year)
+					  filter(?year = ${year})
+					  ?oj  cdm:official-journal_part_of_collection_document <http://publications.europa.eu/resource/authority/document-collection/OJ-${oj}>.
+					  ?w cdm:resource_legal_id_celex ?celex.
+					  ?w cdm:resource_legal_published_in_official-journal ?oj.  
+					  ?e cdm:expression_belongs_to_work ?w.
+					  ?m cdm:manifestation_manifests_expression ?e.
+					  ?m cdm:manifestation_official-journal_part_page_first '${pageNum}'^^xsd:string.
+					}
+					`;
+			
+			sparqlQuery(s_query, function(bindings){
+				var celex = bindings[0].celex.value;
+				var celexMsg = celex+" zkopírován do schránky";
+				if(typeof celex === undefined) return;
+				console.log(celex);
+
+				//create button with celexid and set it up for clipboard copy..					
+				var celexEls = $(`<button class="btn" style="margin-left:5px" data-clipboard-text="${celex}">${celex}</button>`);				
+				celexEls.insertAfter(el);
+
+				var noteEl = el.parent("p.note");	
+				if(noteEl !== null)
+				{
+					var noteLink = noteEl.children('a:first-child');
+					var noteLinkId = noteLink.attr('id');
+					$(`a[href='#${noteLinkId}']`).each(function() 
+													   {
+						//this is a reference of changed notelink.
+						//we will update tooltipster value for all these since tooltipster does copy the html on init.
+						//var instance = $(this).tooltipster('instance');
+						//instance.content(noteEl);
+						$(this).tooltipster('content', noteEl);
+
+					});
+				}
+			});										
 		}
 	});
 	
+	//add tooltip to all notereference links
     $("a").each(function() {
      if(idMatch.test(this.id))//this is correct link.
      {	
@@ -164,5 +165,106 @@
       //we must get the xml since the html does not contain the Page attribute
     }
 	
-    
+	var eclis = [];	
+	$("div#text").find("*").each(function() {
+		var regex = new RegExp("ECLI\\:(.*?)\\:(.*?)\\:(.*?)\\:(\\w+)");
+		
+		$(this).contents().filter(function() {
+				if(this.nodeType != 3) return false;
+				var ismatch= regex.test(this.nodeValue);					
+				if(ismatch) console.log(this.nodeValue);				
+				return  ismatch;
+			}).each(function() {
+			var match = regex.exec(this.nodeValue);
+			eclis.push({ecli:match[0], textNode: this , match:match, processed:false, celex:""});
+			
+		});
+	});
+	
+	if(eclis.length > 0)
+	{
+		debugger;
+		var eclivals = "";
+		eclis.forEach(function(currentValue, index, arr){
+			if(eclivals.length > 0) eclivals+= " , ";
+			eclivals+=`'${currentValue.ecli}'^^xsd:string`;			
+		});
+		
+		var es_query = `prefix cdm: <http://publications.europa.eu/ontology/cdm#> select distinct ?celex, ?ecli where 
+                        {
+                            ?e rdfs:subPropertyOf cdm:ecli.
+                            ?s ?e ?ecli.
+                            filter(?ecli IN (${eclivals}))
+                            ?s cdm:resource_legal_id_celex ?celex.
+                        }`;
+		sparqlQuery(es_query, function(bindings){
+			
+                //assign all found celexes
+				bindings.forEach(function(currentValue, index, arr){
+					console.log(currentValue);
+					var celex = currentValue.celex.value;
+					var ecli = currentValue.ecli.value;
+					
+					//get textnode with ecli.					
+					//replace it with 
+					//create button with celexid and set it up for clipboard copy..							
+					var ecliObject = eclis.filter(function( obj ) {
+						return obj.ecli == ecli;
+					});	
+					
+					ecliObject.forEach(function(currentValue, index, arr){	
+						currentValue.celex = celex;
+					});					
+				});
+			debugger;
+			//go throug all eclis with celex			
+			eclis.filter(function(obj){
+				return obj.celex.length > 0;
+			}).forEach(function(currentValue, index, arr){	
+				if(currentValue.processed === true) return;
+				//now are there other eclis with the same textNode? 
+				var sameTextnodes = eclis.filter(function( obj ) {
+					return obj.textNode === currentValue.textNode;
+				}).sort(function(a, b) {
+					if (a.match.index <  b.mabtch.index) 
+						return -1;
+					if (a.match.index >  b.mabtch.indexn)
+						return 1;
+					return 0;
+				});		
+
+				sameTextnodes.forEach(function(currentValue, index, arr){
+					//now contruct new element.
+					var curNode = currentValue.textNode;
+					var replacementNode = curNode.splitText(currentValue.match.index + currentValue.match[0].length);						
+
+					// adding the span before the text after found ECLI
+					var celexEls = $(`<button class="btn" style="margin-left:5px" data-clipboard-text="${currentValue.celex}">${currentValue.celex}</button>`);
+					//curNode.parentNode.insertBefore(celexEls, replacementNode);
+					celexEls.insertBefore($(replacementNode));
+					
+					currentValue.processed = true;
+				});
+
+					/*celexEls.insertAfter(el);
+
+					var noteEl = el.parent("p.note");	
+					if(noteEl !== null)
+					{
+						var noteLink = noteEl.children('a:first-child');
+						var noteLinkId = noteLink.attr('id');
+						$(`a[href='#${noteLinkId}']`).each(function() 
+														   {
+							//this is a reference of changed notelink.
+							//we will update tooltipster value for all these since tooltipster does copy the html on init.
+							//var instance = $(this).tooltipster('instance');
+							//instance.content(noteEl);
+							$(this).tooltipster('content', noteEl);
+
+						});
+					}*/
+			});
+		});
+	}
+					
 })();
